be7de23a77c66e7e926c8988589cc4977449116c
mizu2256 <165393984+mizu2256@users.noreply.github.com>
2025-08-02 21:07:09 +0900

    headerのレスポンシブ対応_その2

--- kinensai-137th/assets/js/components/header.js
+++ kinensai-137th/assets/js/components/header.js
@@ -1,8 +1,13 @@
 const burger = document.getElementById("burger-menu");
 const nav = document.querySelector(".nav-con");
+const open = document.querySelector(".open-burger");
+const close = document.querySelector(".close-burger");
 
 burger.addEventListener("click", function () {
   nav.classList.toggle("active");
   nav.classList.toggle("show");
   nav.classList.toggle("hide");
+
+  open.classList.toggle("active");
+  close.classList.toggle("active");
 });
